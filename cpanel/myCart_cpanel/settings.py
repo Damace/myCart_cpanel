3344641fce19c45cfad1b7169416db1778158e8a
Alex Mwakalikamo <59507616+Damace@users.noreply.github.com>
2025-04-15 08:07:35 +0300

    debug

--- cpanel/myCart_cpanel/settings.py
+++ cpanel/myCart_cpanel/settings.py
@@ -28,7 +28,6 @@ SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG",cast=bool)
-# DEBUG = True
 ALLOWED_HOSTS = ['mycart-cpanel-1.onrender.com','localhost','127.0.0.1',]
 # ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(",")
 
