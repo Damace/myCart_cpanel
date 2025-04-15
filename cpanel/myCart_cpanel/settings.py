2e551ec9f5ea5558905e89279b467e798ceb82ec
Alex Mwakalikamo <59507616+Damace@users.noreply.github.com>
2025-04-15 07:07:51 +0300

    static_updates

--- cpanel/myCart_cpanel/settings.py
+++ cpanel/myCart_cpanel/settings.py
@@ -28,10 +28,9 @@ SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG",cast=bool)
+# DEBUG = True
 
 ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(",")
-
-# Application definition
 
 
 # Application definition
@@ -85,8 +84,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'myCart_cpanel.wsgi.application'
 
 
+
+
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
+
+
+
 
 DATABASES = {
     'default': {
@@ -132,9 +137,37 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+#STATIC_ROOT =  BASE_DIR / "staticfiles"
+
+STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")] 
+STATIC_ROOT =  os.path.join(BASE_DIR, "staticfiles")
+
+
+
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFileStorage"
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# AUTH_USER_MODEL = 'users.CustomUser'
+# settings.py
+LOGIN_REDIRECT_URL = 'home_page'  # URL name of the home page
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
