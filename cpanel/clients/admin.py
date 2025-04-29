8cf155a540a0700f2750c2e74603ec90f49f6df8
Alex Mwakalikamo <59507616+Damace@users.noreply.github.com>
2025-04-29 15:23:47 +0300

    my_NewUpdate

--- cpanel/clients/admin.py
+++ cpanel/clients/admin.py
@@ -4,7 +4,7 @@ from .models import ClientProfile
 
 @admin.register(ClientProfile)
 class ClientProfileAdmin(admin.ModelAdmin):
-    list_display = ('full_name','phone_number', 'email', 'postal_code', 'address', 'region', ' password')
+    list_display = ('full_name','phone_number', 'email', 'postal_code', 'address','password')
     search_fields = ('full_name','phone_number')
     list_filter = ('full_name','phone_number')
     ordering = ('full_name',)
