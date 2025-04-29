4500535d5a463d60d55881ef3f3d5ca3a53a2adc
Alex Mwakalikamo <59507616+Damace@users.noreply.github.com>
2025-04-29 15:06:41 +0300

    my_clients

--- cpanel/clients/admin.py
+++ cpanel/clients/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
 
-# @admin.register(ClientProfile)
-# class ClientProfileAdmin(admin.ModelAdmin):
-#     list_display = ('full_name', 'email', 'google_uid', 'country', 'region', 'phone_number', 'profile_picture')
-#     search_fields = ('full_name', 'email', 'google_uid', 'country', 'region', 'phone_number')
-#     list_filter = ('country', 'region')
-#     ordering = ('full_name',)
+@admin.register(ClientProfile)
+class ClientProfileAdmin(admin.ModelAdmin):
+    list_display = ('full_name','phone_number', 'email', 'postal_code', 'address', 'region', ' password')
+    search_fields = ('full_name','phone_number')
+    list_filter = ('full_name','phone_number')
+    ordering = ('full_name',)
+
+
+   
+  
